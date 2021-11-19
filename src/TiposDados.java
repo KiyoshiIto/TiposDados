a0cc0bd6c23f59796004db833cb7f65ac2957991
Lucas Ito <ito.lucas@gmail.com>
2021-11-19 17:46:29 -0300

    Initial commit

---
+++ src/TiposDados.java
@@ -0,0 +1,38 @@
+public class TiposDados {
+
+    public static void main(String[] args) {
+        byte b1 = -19;
+        byte b2 = 10;
+
+        short s1 = 10000;
+
+        int i = 100000000;
+
+        long l = 1000000000000000000L;
+
+        float f = 3.14F;
+
+        double d1 = 3.14;
+        double d2 = 3.14D;
+
+        char c = 'w';
+
+        String st = "Olá mundo";
+        boolean bool = true;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c);
+        System.out.println(st);
+        System.out.println(bool);
+
+
+
+    }
+}
