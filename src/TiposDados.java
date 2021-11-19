e10349743b545a0acd05a7527955fec59395e0e0
Lucas Ito <ito.lucas@gmail.com>
2021-11-19 17:56:01 -0300

    Adicionado comentários

--- src/TiposDados.java
+++ src/TiposDados.java
@@ -1,14 +1,20 @@
 public class TiposDados {
 
     public static void main(String[] args) {
+        // dados tipo byte arazenam valores numéricos de -128 até 127
         byte b1 = -19;
         byte b2 = 10;
 
+        // short: -32768 até 32767
         short s1 = 10000;
 
+        // int: -2147483648 até 2137483647
         int i = 100000000;
 
+        // long:- 9223372036854775808 até 9223372036854775807
+        // deve-se utilizar ao final do número "L" ou "l" para definir corretamente como long
         long l = 1000000000000000000L;
+
 
         float f = 3.14F;
 
